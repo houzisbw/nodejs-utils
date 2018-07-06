ad8df79b13a49ce8f2231757dd9bf65299bac5b4
猴子来了 <983134950@qq.com>
2018-07-06 17:05:42 +0800

    Add files via upload

---
+++ getFolderStucture.js
@@ -0,0 +1,40 @@
+var fs = require('fs')
+var path = require('path')
+//获取路径参数下的文件夹结构对象,key为文件名，value为文件路径
+function getDirectoryStucture(directoryPath){
+	var result = {};
+	//同步读取文件内容
+	var files = fs.readdirSync(directoryPath);
+	files.forEach(function(filename){
+		//获取文件完整路径名
+		var fullname = path.join(directoryPath,filename);
+		//获取文件信息对象
+		var fileStat = fs.statSync(fullname)
+		//如果是文件
+		if(fileStat.isFile()){
+			result[filename]=fullname
+		//是文件夹,递归生成文件结构对象
+		}else if(fileStat.isDirectory()){
+			result[filename] = getDirectoryStucture(fullname);
+		}
+	});
+	return result
+}
+//获取js对象深度,{}深度为0
+function getDepthOfObject(obj){
+	function helper(obj){
+		if(!obj||Object.prototype.toString.call(obj)!=='[object Object]')return 0
+		var keys = Object.keys(obj);
+		if(keys.length===0)return 0;
+		var arr = []
+		for(var i=0;i<keys.length;i++){
+			arr.push(helper(obj[keys[i]]))
+		}
+		return Math.max.apply(this,arr)+1
+	}
+	return helper(obj)
+
+}
+
+exports.getDirectoryStucture = getDirectoryStucture;
+exports.getDepthOfObject = getDepthOfObject;
